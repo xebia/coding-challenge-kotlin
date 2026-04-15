7dab226320038df3428913d107d3ef75b1b78b04
Ruben Oostinga <roostinga@xebia.com>
2026-04-15 15:36:36 +0200

    Remove solution

--- src/main/kotlin/PascalsTriangle.kt
+++ src/main/kotlin/PascalsTriangle.kt
@@ -1,18 +1,6 @@
 class PascalsTriangle {
 
     fun triangleRow(n: Int): List<Int> {
-        if (n == 0) {
-            return listOf(1)
-        }
-
-        val previousRow = triangleRow(n - 1)
-
-        val row = mutableListOf(1)
-        for (col in 1 until n) {
-            row.add(previousRow[col - 1] + previousRow[col] + 1)
-        }
-        row.add(1)
-
-        return row
+        TODO("Not implemented yet")
     }
 }
